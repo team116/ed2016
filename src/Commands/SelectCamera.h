7e8466d20fb2bd2967a729dd91c60d4aa2c82661
Will Carty <cartycrabber@gmail.com>
2016-01-16 00:28:41 -0500

    Included Camera subsystem in SelectCamera command

--- src/Commands/SelectCamera.h
+++ src/Commands/SelectCamera.h
@@ -1,8 +1,9 @@
 #ifndef SelectCamera_H
 #define SelectCamera_H
 
-#include "../CommandBase.h"
-#include "WPILib.h"
+#include <CommandBase.h>
+#include <WPILib.h>
+#include <Subsystems/Cameras.h>
 
 class SelectCamera: public CommandBase
 {
