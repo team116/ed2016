476bdea224ecf12f2d46dd93395831fda4b5f3a2
Amanzeb <amanymous@gmail.com>
2016-01-18 12:54:05 -0500

    Created instances of joysticks and buttons in OI

--- src/OI.cpp
+++ src/OI.cpp
@@ -5,9 +5,27 @@ OI* OI::INSTANCE = nullptr;
 
 OI::OI()
 {
+	joystick_left = new Joystick(0);
+	joystick_right = new Joystick(1);
+	joystick2 = new Joystick(2);
+	intake_in = new JoystickButton(joystick2,1);
+	intake_out = new JoystickButton(joystick2,2);
+	shoot = new JoystickButton(joystick2,3);
+	aim_shooter_up = new JoystickButton(joystick2,4);
+	aim_shooter_down = new JoystickButton(joystick2,5);
+	extend_arm = new JoystickButton(joystick2,6);
+	retrive_arm = new JoystickButton(joystick2,7);
+	arm_claw_open_close = new JoystickButton(joystick2,8);
 	// Process operator interface input here.
 }
-
+float OI::GetJoystickLeftY()
+{
+	return joystick_left->GetY();
+}
+float OI::GetJoystickRightY()
+{
+	return joystick_right->GetY();
+}
 OI* OI::getInstance()
 {
 	if (INSTANCE != nullptr)
