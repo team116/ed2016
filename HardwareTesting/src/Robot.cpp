5323796583332c378d1b1e0ee02699e6ff4aea13
Calcifer <lordofthemaracas@gmail.com>
2016-02-21 09:03:21 -0500

    HardwareTesting now zero indexes ports, makes better use of for loops + arrays, & has a button to go backwards

--- HardwareTesting/src/Robot.cpp
+++ HardwareTesting/src/Robot.cpp
@@ -22,12 +22,12 @@ public:
 		timer->Start();
 		timer->Reset();
 
-		int port = 1;
+		int port = 0;
 		victor = new VictorSP(port);
 
 		while (port <= 13 && IsAutonomous() && IsEnabled())
 		{
-			if (timer->HasPeriodPassed(1.0))
+			if (timer->Get() > 1.0)
 			{
 				victor->Set(-1.0);
 			}
@@ -37,7 +37,7 @@ public:
 			}
 
 
-			if (timer->HasPeriodPassed(2.0))
+			if (timer->Get() > 2.0)
 			{
 				++port;
 				victor->Set(0.0);
@@ -50,127 +50,55 @@ public:
 		delete victor;
 	}
 
-	void OperatorControl() // Teleop
+	void OperatorControl()
 	{
-		VictorSP* victor1 = new VictorSP(1);
-		VictorSP* victor2 = new VictorSP(2);
-		VictorSP* victor3 = new VictorSP(3);
-		VictorSP* victor4 = new VictorSP(4);
-		VictorSP* victor5 = new VictorSP(5);
-		VictorSP* victor6 = new VictorSP(6);
-		VictorSP* victor7 = new VictorSP(7);
-		VictorSP* victor8 = new VictorSP(8);
-		VictorSP* victor9 = new VictorSP(9);
-		VictorSP* victor10 = new VictorSP(10);
-		VictorSP* victor11 = new VictorSP(11);
-		VictorSP* victor_1= new VictorSP(12);
-		VictorSP* victor_2 = new VictorSP(13);
+		VictorSP* victors[13];
+		JoystickButton* buttons[13];
+
+		Joystick joystick1(0);
+		Joystick joystick2(1);
+		JoystickButton reverse_button(&joystick1, 11);
+
+		int i;
+		for (i = 0; i < 13; ++i)
+		{
+			if (i < 10)
+			{
+				buttons[i] = new JoystickButton(&joystick1, i + 1);
+			}
+			else
+			{
+				buttons[i] = new JoystickButton(&joystick2, i - 9);
+			}
+			victors[i] = new VictorSP(i);
+		}
+
 		while (IsOperatorControl() && IsEnabled())
 		{
-			if (joystick1->GetRawButton(1))
+			for (i = 0; i < 13; ++i)
 			{
-				victor1->Set(1.0);
+				if (buttons[i]->Get())
+				{
+					if (reverse_button.Get())
+					{
+						victors[i]->Set(-1.0);
+					}
+					else
+					{
+						victors[i]->Set(1.0);
+					}
+				}
+				else
+				{
+					victors[i]->Set(0.0);
+				}
 			}
-			else
-			{
-				victor1->Set(0.0);
-			}
-			if (joystick1->GetRawButton(2))
-			{
-				victor2->Set(1.0);
-			}
-			else
-			{
-				victor2->Set(0.0);
-			}
-			if (joystick1->GetRawButton(3))
-			{
-				victor3->Set(1.0);
-			}
-			else
-			{
-				victor3->Set(0.0);
-			}
-			if (joystick1->GetRawButton(4))
-			{
-				victor4->Set(1.0);
-			}
-			else
-			{
-				victor4->Set(0.0);
-			}
-			if (joystick1->GetRawButton(5))
-			{
-				victor5->Set(1.0);
-			}
-			else
-			{
-				victor5->Set(0.0);
-			}
-			if (joystick1->GetRawButton(6))
-			{
-				victor6->Set(1.0);
-			}
-			else
-			{
-				victor6->Set(0.0);
-			}
-			if (joystick1->GetRawButton(7))
-			{
-				victor7->Set(1.0);
-			}
-			else
-			{
-				victor7->Set(0.0);
-			}
-			if (joystick1->GetRawButton(8))
-			{
-				victor8->Set(1.0);
-			}
-			else
-			{
-				victor8->Set(0.0);
-			}
-			if (joystick1->GetRawButton(9))
-			{
-				victor9->Set(1.0);
-			}
-			else
-			{
-				victor9->Set(0.0);
-			}
-			if (joystick1->GetRawButton(10))
-			{
-				victor10->Set(1.0);
-			}
-			else
-			{
-				victor10->Set(0.0);
-			}
-			if (joystick1->GetRawButton(11))
-			{
-				victor11->Set(1.0);
-			}
-			else
-			{
-				victor11->Set(0.0);
-			}
-			if (joystick2->GetRawButton(12))
-			{
-				victor_1->Set(1.0);
-			}
-			else
-			{
-				victor_1->Set(0.0);
-			}
-			if (joystick2->GetRawButton(13))
-			{
-				victor_2->Set(1.0);
-			}
-			else
-			{
-				victor_2->Set(0.0);
-			}
+		}
+
+		for (i = 0; i < 13; ++i)
+		{
+			delete victors[i];
+			delete buttons[i];
 		}
 	}
 
