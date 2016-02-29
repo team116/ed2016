b0ddb0733c1e675eabbd2f6b8220598ebdd22290
Calcifer <lordofthemaracas@gmail.com>
2016-02-28 23:56:17 -0500

    added command to move the holder wheels, mostly for testing purposes

---
+++ ed2016/src/Commands/MoveHolderWheel.cpp
@@ -0,0 +1,38 @@
+#include <Commands/MoveHolderWheel.h>
+
+MoveHolderWheel::MoveHolderWheel(Utils::HorizontalDirection dir)
+{
+	Requires(&*holder_wheel);
+	direction = dir;
+}
+
+// Called just before this Command runs the first time
+void MoveHolderWheel::Initialize()
+{
+	holder_wheel->setWheelDirection(direction);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void MoveHolderWheel::Execute()
+{
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool MoveHolderWheel::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void MoveHolderWheel::End()
+{
+	holder_wheel->setWheelDirection(Utils::HorizontalDirection::H_STILL);
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void MoveHolderWheel::Interrupted()
+{
+	End();
+}
