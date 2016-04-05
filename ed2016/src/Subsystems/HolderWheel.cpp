916d92d3641d609f2da9ea14ed0aecec66c7bca3
Calcifer <lordofthemaracas@gmail.com>
2016-04-04 22:57:39 -0400

    new speeds for holder wheel

--- ed2016/src/Subsystems/HolderWheel.cpp
+++ ed2016/src/Subsystems/HolderWheel.cpp
@@ -19,11 +19,11 @@ void HolderWheel::setWheelDirection(Utils::HorizontalDirection direction)
 	wheel_direction = direction;
 	if (direction == Utils::HorizontalDirection::IN)
 	{
-		holder_wheel->Set(-1.0);
+		holder_wheel->Set(-0.40);
 	}
 	else if (direction == Utils::HorizontalDirection::OUT)
 	{
-		holder_wheel->Set(1.0);
+		holder_wheel->Set(0.5);
 	}
 	else
 	{
